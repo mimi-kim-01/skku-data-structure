cedfae6600d845ea4a4daacc4a863da10f1c95b6
mimi-kim-01 <mimi010912@g.skku.edu>
2022-04-20 23:41:25 +0900

    start

---
+++ binarysearchtree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//binary search tree
+typedef struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}Node;
+
+//functions
+
+//main
+
+//functions specific
