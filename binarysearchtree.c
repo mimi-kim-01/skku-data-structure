b5b23243cd5315a7dd0e6be111bb54bf58b98b67
mimi-kim-01 <mimi010912@g.skku.edu>
2022-04-20 23:47:49 +0900

    hh

--- binarysearchtree.c
+++ binarysearchtree.c
@@ -4,13 +4,33 @@
 
 //binary search tree
 typedef struct Node{
-    int data;
+    int value;
     struct Node* left;
     struct Node* right;
 }Node;
 
 //functions
+void insert_node(Node* root, int value);
+void inorder_traversal(Node* root);
+void right_root_left_traversal(Node* root);
+int get_min(Node* root);
+int get_max(Node* root);
+int height(Node* bst);
+Node* find_node(Node* root, int value);
+Node* get_right_child(Node* root, int value);
+Node* get_left_child(Node* root, int value);
+void count_node(Node* root);
+void delete_node(Node* root, int value);
+void print_bst(Node* root);
+void clear(Node* root);
 
 //main
+int main(){
+    Node* root = (Node*)malloc(sizeof(Node));
+
+
+    free(root);
+    return 0;
+}
 
 //functions specific
