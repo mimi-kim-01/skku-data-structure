c98c575ea8e048cec1c5c4b5fc5dd6b96320bac8
mimi-kim-01 <mimi010912@g.skku.edu>
2022-04-21 11:43:21 +0900

    22

--- binarysearchtree.c
+++ binarysearchtree.c
@@ -9,6 +9,8 @@ typedef struct Node{
     struct Node* right;
 }Node;
 
+int num = 0;
+
 //functions
 void insert_node(Node* root, int value);
 void inorder_traversal(Node* root);
@@ -34,3 +36,44 @@ int main(){
 }
 
 //functions specific
+void insert_node(Node* root, int value){
+    if (num == 0) {
+        printf("THE BST IS EMPTY!\n");
+        return;
+    }
+    Node* new = (Node*)malloc(sizeof(Node));
+    new->value = value;
+    new->left = NULL;
+    new->right = NULL;
+    if (root->value < value){
+        if (root->right == NULL){
+            root->right = new;
+            num++;
+        }
+        else insert_node(root->right, value);
+    }
+    else if (root->value > value){
+        if (root->left == NULL){
+            root->left = new;
+            num++;
+        }
+        else insert_node(root->left, value);
+    }
+    else{
+        printf("[%d] ALREADY EXISTS!\n", value);
+        return;
+    }
+}
+
+void inorder_traversal(Node* root);
+void right_root_left_traversal(Node* root);
+int get_min(Node* root);
+int get_max(Node* root);
+int height(Node* bst);
+Node* find_node(Node* root, int value);
+Node* get_right_child(Node* root, int value);
+Node* get_left_child(Node* root, int value);
+void count_node(Node* root);
+void delete_node(Node* root, int value);
+void print_bst(Node* root);
+void clear(Node* root);
