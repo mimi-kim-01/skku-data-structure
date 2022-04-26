afb1aa737cfaacbc1ee2721ac33df0efd2057101
mimi-kim-01 <mimi010912@g.skku.edu>
2022-04-27 01:41:01 +0900

    sad

--- avltree.c
+++ avltree.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 
 //avl tree
+typedef struct Node{
+    char data;
+    struct Node* left;
+    struct Node* right;
+    struct Node* parent;
+    int degree;
+    int level;
+}Node;
+
+typedef struct Tree{
+    Node* root;
+    int num;
+}Tree;
 
 //functions
 
