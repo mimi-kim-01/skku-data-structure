5434b2b8cff458e6a79d9f262ef6b8fdbc8889f4
mimi-kim-01 <mimi010912@g.skku.edu>
2022-05-03 02:08:06 +0900

    hh

--- queuetie.py
+++ queuetie.py
@@ -1,9 +1,9 @@
 #user
 class User:
     def __init__(self, name, phone, magic):
-        self.name = None
-        self.phone = None
-        self.magic = None
+        self.name = name
+        self.phone = phone
+        self.magic = magic
         self.prev = None
         self.next = None
 
@@ -17,13 +17,31 @@ class Queue:
         self.cur2 = self.head #no magic pass
     
     #functions
-    def enqueue(self, new): #처음에 enqueue하는거 따로 만들기??
+    def create(self, new):
+        self.head = new
         if new.magic:
+            self.cur1 = new
+            self.len1 += 1
+        self.cur2 = new
+        self.len2 += 1
+
+    def enqueue(self, new): #cur1이랑 cur2연결이 잘 안되는듯, len1==len2일떄 next연결이 잘 안됨
+        if self.len2 == 0:
+            self.create(new)
+            return
+        if new.magic:
+            self.cur1.next = new
+            self.cur1.prev = self.cur1
             new.prev = self.cur1
             new.next = self.cur1.next
             self.cur1 = new
             self.len1 += 1
         else: 
+            if self.len2 == self.len1:
+                self.cur1.next = new
+                new.prev = self.cur1
+            self.cur2.next = new
+            self.cur2.prev = self.cur2
             new.prev = self.cur2
             new.next = None
             self.cur2 = new
@@ -68,13 +86,13 @@ class Queue:
 
 #ride
 class Ride:
-    def __init__(self, queue):
+    def __init__(self, queue, num, time):
         self.line = queue
         self.time1 = None #waiting time for no magic pass
         self.time2 = None #waiting time for magic pass
-        self.num = None #num per ride
-        self.time = None #time per ride (include break time)
+        self.num = num #num per ride
+        self.time = time #time per ride (include break time)
     
-    def time(self):
-        self.time1 = (self.line.len1 / self.num) * self.time
-        self.time2 = (self.line.len2 / self.num) * self.time
+    def get_time(self):
+        self.time1 = int((self.line.len1 / self.num) * self.time)
+        self.time2 = int((self.line.len2 / self.num) * self.time)
