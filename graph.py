52ab4ce6f9fe200855bf8b8b072b0cdf6c0a4f7a
mimi-kim-01 <mimi010912@g.skku.edu>
2022-05-05 02:41:55 +0900

    start

--- graph.py
+++ graph.py
@@ -2,7 +2,19 @@ class Graph:
     def __init__(self):
         self.graph = {}
 
-    def add_vertex(self, vertex):
+    def insert_vertex(self, vertex):
         self.graph[vertex] = []
     
+    def add_edge(self, one, two):
+        self.graph[one].append(two)
+        self.graph[two].append(one)
     
+    def degree_of_vertex(self, vertex):
+        degree = len(self.graph[vertex])
+        return degree
+    
+    def is_connected(self):
+        #모르겟다
+    
+    def is_empty(self):
+        
